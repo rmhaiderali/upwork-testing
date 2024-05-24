f3bf711d5597644f538d73a7c2c5d126c54c3d61
Haider Ali <rmha64@gmail.com>
2024-05-24 22:14:37 +0500

    Add gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
+    base: "/upwork-testing/",
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
